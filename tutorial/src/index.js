8e56ad6b7824bf3d629c0ade96e6290da4c897a3
Piotr <p.skolorzynski@gmail.com>
2021-08-24 18:03:59 +0200

    first component in details

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,10 +1,20 @@
-// import React from 'react'; - od wersji 17 nie ma konieczności importowania Reacta
+// import React from 'react'; //- od wersji 17 nie ma konieczności importowania Reacta
 import ReactDom from 'react-dom';
 
 function Greeting() {
-    return <h4>this is Piotr and this is my first component</h4>;
+    return (
+        <div>
+            <h1>hello, world!</h1>
+        </div>
+    );
 }
 
+// //powód dla którego warto pisać z użyciem JSX jest poniżej (straszny bałagan przy użycziu React.createElement). Do tego jeszcze jedna rzecz - jeśli używasz React.createElement to musisz go zaimportować
+// const Greeting = () => {
+//     return React.createElement('div', {}, React.createElement('h1', {}, 'hello, world!')); 
+// }
+ 
+export default Greeting
 ReactDom.render(<Greeting />, document.getElementById('root'));
 
 // tagi trzeba zawsze zamykać jak powyżej albo jak w HTML 5 <Greeting></Greeting>

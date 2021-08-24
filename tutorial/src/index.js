ed902ac364ace231cb78cb97e77bc2a94fb5620b
Piotr <p.skolorzynski@gmail.com>
2021-08-24 18:23:56 +0200

    JSX rules

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,33 @@
 // import React from 'react'; - od wersji 17 nie ma konieczności importowania Reacta
 import ReactDom from 'react-dom';
 
+// JSX rules:
+
+//return always element - div/ section/ article or Fragment(React.Fragment lub <></>)
+
+//use camelCase for html attribute
+
+// className instead fo html class
+
+//close every element
+
+//formating
+
+
+
 function Greeting() {
-    return <h4>this is Piotr and this is my first component</h4>;
+    return (
+        <div>
+            <h3>hello people</h3>
+            <ul>
+                <li>
+                    <a href="/">hello world</a>
+                </li>
+            </ul>
+            <img src="" alt="" />
+            <input type="text" />
+        </div>
+    );
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));

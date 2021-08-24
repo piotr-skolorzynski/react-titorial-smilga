91600ab860471cde7e21b3615b61e15cbca0e2b6
Piotr <p.skolorzynski@gmail.com>
2021-08-24 18:58:54 +0200

    nested components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,34 +1,19 @@
 // import React from 'react'; //- od wersji 17 nie ma konieczności importowania Reacta
 import ReactDom from 'react-dom';
 
-// JSX rules:
-
-//return always element - div/ section/ article or Fragment(React.Fragment lub <></>)
-
-//use camelCase for html attribute
-
-// className instead fo html class
-
-//close every element
-
-//formating
-
-
+//nested components and react tools
 
 function Greeting() {
     return (
         <div>
-            <h3>hello people</h3>
-            <ul>
-                <li>
-                    <a href="/">hello world</a>
-                </li>
-            </ul>
-            <img src="" alt="" />
-            <input type="text" />
+            <Person />
+            <Message />
         </div>
     );
 }
 
-export default Greeting
+const Person = () => <h2>piotr skołorzyński</h2>;
+
+const Message = () => <p>this is my message</p>;
+
 ReactDom.render(<Greeting />, document.getElementById('root'));

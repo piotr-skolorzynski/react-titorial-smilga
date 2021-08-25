11e6ed1793ad7a85a3aedde3223bd69779cd29df
Piotr <p.skolorzynski@gmail.com>
2021-08-25 10:44:24 +0200

    modify index.css move inline style from index.js modify index.js file add example of using javascript in JSX

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,36 +4,47 @@ import ReactDom from 'react-dom';
 //importing css
 import './index.css';
 
-//nested components and react tools
-
 function BookList() {
     return (
         <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
             <Book />
         </section>
     );
 }
 
+const author = 'Brandon Sanderson'; // zmienna z autorem, wyprowadzenie poza komponent też zadziała bo jest to wtedy zmienna globalna
+
+//PAMIĘTAJ przekazujemy w JSX coś co zawsze zwaraca wartość (expression) np. {6+6}, dlatego nie można przekazać np. czegoś takiego { let x = 6} czyli statement
 const Book = () => {
+    const title = 'Elantris: wydanie specjalne Twarda oprawa'; //zapisanie tytułu w zmiennej a następnie wstrzyknięcie do h4
     return (
         <article className="book">
-            <Image />
-            <Title />
-            <Author />
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51rExxvZB+L._SX315_BO1,204,203,200_.jpg" alt="book" />
+            <h1>{ title }</h1>
+            <h4>{ author.toUpperCase() }</h4>
         </article>
     )
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51rExxvZB+L._SX315_BO1,204,203,200_.jpg" alt="book" />;
+//wcześniejszy przykład rozbijający komponenty na kawałki oraz przykład css
 
-const Title = () => <h1>Elantris: wydanie specjalne Twarda oprawa</h1>;
+// const Book = () => {
+//     return (
+//         <article className="book">
+//             <Image />
+//             <Title />
+//             <Author />
+//         </article>
+//     )
+// }
 
-//poniżej przykład wstawiania inlinowego stylu , jest to widoczne na stronie, wpisuje się to jak obiekt bo działamy w javascripcie oraz obowiązuje camelCase jak w vanilli
-const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Brandon Sanderson</h4>;
+// const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51rExxvZB+L._SX315_BO1,204,203,200_.jpg" alt="book" />;
+
+// const Title = () => <h1>Elantris: wydanie specjalne Twarda oprawa</h1>;
+
+// //poniżej przykład wstawiania inlinowego stylu , jest to widoczne na stronie, wpisuje się to jak obiekt bo działamy w javascripcie oraz obowiązuje camelCase jak w vanilli
+// const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Brandon Sanderson</h4>;
+
+
 
 ReactDom.render(<BookList />, document.getElementById('root'));

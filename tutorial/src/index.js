b32b12139625ce2b734286e54d6dd4e741f2afc5
Piotr <p.skolorzynski@gmail.com>
2021-08-25 11:43:51 +0200

    modify index.js file add example of using props to use dynamic values in components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,28 +4,83 @@ import ReactDom from 'react-dom';
 //importing css
 import './index.css';
 
+//props - taka nazwa jest konwencją
+//komponenty to funkcje więc można przekazywać do niej argumenty i wykorzystywać komponent do pokazywania różnych treści na bazie tego samego wyglądu
+
 function BookList() {
     return (
         <section className="booklist">
-            <Book />
+            <Book 
+                img={ firstBook.img } 
+                title={ firstBook.title } 
+                author={ firstBook.author }
+            /> {/* props przekazujemy przy renderowaniu komponentu */}
+            <Book 
+                img={ secondBook.img } 
+                title={ secondBook.title } 
+                author={ secondBook.author }
+            />
         </section>
     );
 }
 
-const author = 'Brandon Sanderson'; // zmienna z autorem, wyprowadzenie poza komponent też zadziała bo jest to wtedy zmienna globalna
+// setup vars, zapisz zmienne jako obiekt
+
+const firstBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51rExxvZB+L._SX315_BO1,204,203,200_.jpg',
+    title: 'Elantris: wydanie specjalne Twarda oprawa',
+    author: 'Brandon Sanderson'
+}
+
+const secondBook = {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81cJzGY%2BJJL._AC_UL200_SR200,200_.jpg',
+    title: 'Leadership without easy answers',
+    author: 'Ronald A. Heifetz'
+}
 
 //PAMIĘTAJ przekazujemy w JSX coś co zawsze zwaraca wartość (expression) np. {6+6}, dlatego nie można przekazać np. czegoś takiego { let x = 6} czyli statement
-const Book = () => {
-    const title = 'Elantris: wydanie specjalne Twarda oprawa'; //zapisanie tytułu w zmiennej a następnie wstrzyknięcie do h4
+
+const Book = (props) => {
+        console.log(props);
+
     return (
         <article className="book">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51rExxvZB+L._SX315_BO1,204,203,200_.jpg" alt="book" />
-            <h1>{ title }</h1>
-            <h4>{ author.toUpperCase() }</h4>
+            <img src={ props.img } alt="book" />
+            <h1>{ props.title }</h1>
+            <h4>{ props.author }</h4>
         </article>
     )
 }
 
+ReactDom.render(<BookList />, document.getElementById('root'));
+
+//***********************************************************************************************
+
+// przekazywanie zmiennych w JSX
+// function BookList() {
+//     return (
+//         <section className="booklist">
+//             <Book />
+//         </section>
+//     );
+// }
+
+// const author = 'Brandon Sanderson'; // zmienna z autorem, wyprowadzenie poza komponent też zadziała bo jest to wtedy zmienna globalna
+
+// //PAMIĘTAJ przekazujemy w JSX coś co zawsze zwaraca wartość (expression) np. {6+6}, dlatego nie można przekazać np. czegoś takiego { let x = 6} czyli statement
+// const Book = () => {
+//     const title = 'Elantris: wydanie specjalne Twarda oprawa'; //zapisanie tytułu w zmiennej a następnie wstrzyknięcie do h4
+//     return (
+//         <article className="book">
+//             <img src="https://images-na.ssl-images-amazon.com/images/I/51rExxvZB+L._SX315_BO1,204,203,200_.jpg" alt="book" />
+//             <h1>{ title }</h1>
+//             <h4>{ author.toUpperCase() }</h4>
+//         </article>
+//     )
+// }
+
+
+//***************************************************************************
 //wcześniejszy przykład rozbijający komponenty na kawałki oraz przykład css
 
 // const Book = () => {
@@ -44,7 +99,3 @@ const Book = () => {
 
 // //poniżej przykład wstawiania inlinowego stylu , jest to widoczne na stronie, wpisuje się to jak obiekt bo działamy w javascripcie oraz obowiązuje camelCase jak w vanilli
 // const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Brandon Sanderson</h4>;
-
-
-
-ReactDom.render(<BookList />, document.getElementById('root'));
